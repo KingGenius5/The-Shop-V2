c5e0885d637678ce177fb81ace28c5a4c13ddb51
Muhammad Tifak <moht20009@gmail.com>
2019-10-16 22:41:47 -0700

    Created cart routes

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/my_app_db')
 client = MongoClient(host=f"{host}?retryWrites=false")
 db = client.get_default_database()
 
+#drop function in python helps remove rows based on labels/tags rather than numeric indexing, this will be great for our cart
 players_collection = db.players
+players_collection.drop()
+carts = db.carts
+carts.drop()
+
+
+db.players.insert_many([{'player': 'Lebron James', 'charity': 'Lebron James Foundation', 'pledge': 250, 'image': 'http://ih.constantcontact.com/fs054/1107137834319/img/72.jpg?a=1109948824771'},
+                        {'player': 'Serge Ibaka', 'charity': 'Serge Ibaka Foundation', 'pledge': 150, 'image': 'https://pbs.twimg.com/profile_images/570015235500838912/s49_Or4n_400x400.jpeg'}])
 
 @app.route('/')
 def index():
@@ -69,6 +77,52 @@ def delete_player(player_id):
     """Delete a player."""
     players_collection.delete_one({'_id': ObjectId(player_id)})
     return redirect(url_for('index'))
+
+"""
+********** FOR BUILDING CART FUNCTION *********
+"""
+
+@app.route('/player/<player_id>/add', methods=['POST'])
+def add_to_cart(player_id):
+    if carts.find_one({'_id': ObjectId(player_id)}):
+        carts.update_one(
+            {'_id': ObjectId(player_id)},
+            {'$inc': {'quantity': int(1)}}
+        )
+    else:
+        carts.insert_one(
+            {**players_collection.find_one({'_id': ObjectId(player_id)}), **{'quantity': 1}})
+
+    return redirect(url_for('show_cart'))
+
+@app.route('/cart')
+def show_cart():
+    """Show cart."""
+    cart = carts.find()
+    # This will display all products by looping through the database
+    total_price = list(carts.find({}))
+    total = 0
+    for i in range(len(total_price)):
+        total += total_price[i]["price"]*total_price[i]["quantity"]
+        round(float(total), 2)
+
+    return render_template('cart.html', carts=cart, total=total)
+
+
+@app.route('/carts/<cart_id>/delete', methods=['POST'])
+def remove_from_cart(cart_id):
+    # This will delete a product by using an id as a parameter
+    """Remove one product from cart"""
+    cart_item  = carts.find_one({'_id': ObjectId(cart_id)})
+    carts.update_one(
+        {'_id': ObjectId(cart_id)},
+        {'$inc': {'quantity': -int(1)}}
+    )
+
+    if cart_item['quantity']==1:
+        carts.remove({'_id': ObjectId(cart_id)})
+
+    return redirect(url_for('show_cart'))
 
 
 if __name__ == '__main__':
