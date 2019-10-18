c37573c8636117054e11903b7694ad4680854ada
Muhammad Tifak <moht20009@gmail.com>
2019-10-18 11:57:10 -0700

    Fixing concatenation error

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def show_cart():
     total = 0
     print("showing cart")
     for i in range(len(total_price)):
-        total += total_price[i]["price"]*total_price[i]["quantity"]
+        total += int(total_price[i]["price"])*int(total_price[i]["quantity"])
         round(float(total), 2)
 
     return render_template('cart.html', carts=cart, total=total)
