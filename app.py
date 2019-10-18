1fe58f97d8b012a95848b6647d119684588cc9a0
Muhammad Tifak <moht20009@gmail.com>
2019-10-18 08:01:58 -0700

    Testing deployment again

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ db = client.get_default_database()
 players = db.players
 comments = db.comments
 
-cart = db.cart
+carst = db.carts
 players.drop()
-cart.drop()
+carts.drop()
 
 app = Flask(__name__)
 
@@ -98,28 +98,45 @@ def update_player(player_id):
 ********** FOR BUILDING CART FUNCTION *********
 """
 
+@app.route('/players/<player_id>/add', methods=['POST'])
+def add_to_cart(player_id):
+    if carts.find_one({'_id': ObjectId(player_id)}):
+        carts.update_one(
+            {'_id': ObjectId(player_id)},
+            {'$inc': {'quantity': int(1)}}
+        )
+    else:
+        carts.insert_one(
+            {**players.find_one({'_id': ObjectId(player_id)}), **{'quantity': 1}})
+
+    return redirect(url_for('show_cart'))
+
 @app.route('/cart')
 def show_cart():
     """Show cart."""
-    carts = cart.find()
+    cart = carts.find()
+    # This will display all products by looping through the database
+    total_price = list(carts.find({}))
+    total = 0
+    for i in range(len(total_price)):
+        total += total_price[i]["price"]*total_price[i]["quantity"]
+        round(float(total), 2)
 
-    return render_template('cart.html', cart=carts)
+    return render_template('cart.html', carts=cart, total=total)
 
-@app.route('/cart', methods=['POST'])
-def add_to_cart():
-    '''Submit new item to cart'''
-    player = {
-        'title': request.form.get('title'),
-        "price": request.form.get('price')
-    }
 
-    add_item = cart.insert_one(player).inserted_id
-    return redirect(url_for('show_cart', add_item=add_item))
+@app.route('/carts/<cart_id>/delete', methods=['POST'])
+def remove_from_cart(cart_id):
+    # This will delete a product by using an id as a parameter
+    """Remove one product from cart"""
+    cart_item  = carts.find_one({'_id': ObjectId(cart_id)})
+    carts.update_one(
+        {'_id': ObjectId(cart_id)},
+        {'$inc': {'quantity': -int(1)}}
+    )
 
-@app.route('/cart/<player_id>/delete', methods=['POST'])
-def remove_from_cart(player_id):
-    '''Remove item from cart'''
-    cart.delete_one({'_id': ObjectId(player_id)})
+    if cart_item['quantity']==1:
+        carts.remove({'_id': ObjectId(cart_id)})
 
     return redirect(url_for('show_cart'))
 
